7f2a10f97f525eeb49ece76c35034f5455710ace
Srishanth3524 <srishanthreddy3524@gmail.com>
2026-04-07 14:26:19 +0530

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,9 @@ import reflex as rx
 
 config = rx.Config(
        app_name="RAG_PROJECT",
-       api_url="https://rag-project-neon-wood.reflex.run",
+       api_url="0351582a-27bd-4bd5-ae6f-26954d44a4a6.fly.dev",
+       plugins=[
+           rx.plugins.SitemapPlugin(),
+           rx.plugins.TailwindV4Plugin(),
+       ]
 )
